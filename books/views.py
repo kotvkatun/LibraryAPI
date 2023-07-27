7792a884658d1a97d1f7103a5bfe45056499fc35
krskv <karsakovt89@gmail.com>
2023-07-27 21:55:01 +0300

    Add apis app

--- books/views.py
+++ books/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic import ListView
 from .models import Book
+
 
 class BookListView(ListView):
     model = Book
